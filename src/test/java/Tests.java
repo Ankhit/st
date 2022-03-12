8e0443cb4ba1fceab5ff256c46913d16616a002b
shoaib <shoaibmuhammad2003@gmail.com>
2022-03-12 15:46:51 +0100

    all test success

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -15,21 +15,21 @@ import static org.junit.Assert.assertEquals;
         @Test
         //Test case by Ankhit Tangella
         public void testAdd() {
-            assertEquals(14, calc.add(7, 3));
+            assertEquals(10, calc.add(7, 3));
         }
 
         @Test
         //Test case by Trayi Chaganti
         public void testSub() {
-            assertEquals(5, calc.sub(4, 3));
+            assertEquals(1, calc.sub(4, 3));
         }
         @Test
         //Test case by Bhavya Yaddula
-        public void testDiv() {assertEquals(13, calc.div(9, 9));}
+        public void testDiv() {assertEquals(1, calc.div(9, 9));}
 
         @Test
         //Test case by Shoaib Anwar
-        public void testMult() {assertEquals(8,  calc.mult(9, 9));}
+        public void testMult() {assertEquals(81,  calc.mult(9, 9));}
 
 
 
